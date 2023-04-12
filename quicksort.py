c9a87cef0a54ef710ffd2b0062bb66faa29aed95
malymary <malyutina.mary@gmail.com>
2023-04-13 00:59:18 +0800

    comments for python quicksort

--- quicksort.py
+++ quicksort.py
@@ -4,16 +4,19 @@ array_1 = [1, 5, 6, 8, 9, 3]
 
 
 def quicksort(arr):
-
+    # set base case
     if len(arr) < 2:
         return arr
 
+    # set recursive case
     else:
+        # choose the first element of array as pivot
         pivo = arr[0]
-
+        # write all elements of array smaller than pivot to a new array
         left = [i for i in arr[1:] if i <= pivo]
+        # write all elements of array greater than pivot to a new array
         right = [i for i in arr[1:] if i > pivo]
-
+        # return smaller elements, pivot, and greater elements
         return quicksort(left) + [pivo] + quicksort(right)
 
 

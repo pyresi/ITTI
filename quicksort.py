5fb2f22a7722cfb4761adc907a8c63e9dca7ed08
malymary <malyutina.mary@gmail.com>
2023-04-13 23:33:48 +0800

    Co-authored-by: Anastasiia Pyresina <pyresi@users.noreply.github.com>

--- quicksort.py
+++ quicksort.py
@@ -21,3 +21,29 @@ def quicksort(arr):
 
 
 print(quicksort(array_1))
+
+'''
+One potential improvement is to use Python's built-in sorting function sorted()
+for smaller arrays rather than recursively calling quickSort. This would reduce
+the overhead of function calls and improve performance for small arrays.
+
+In this optimized version, if either the left or right array has less than 10 elements,
+sorted() is used to sort it directly. This threshold value can be adjusted based on
+performance requirements and input data.
+'''
+def quickSort(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[0]
+        left = [x for x in arr[1:] if x < pivot]
+        right = [x for x in arr[1:] if x >= pivot]
+        if len(left) < 10:
+            left = sorted(left)
+        else:
+            left = quickSort(left)
+        if len(right) < 10:
+            right = sorted(right)
+        else:
+            right = quickSort(right)
+        return left + [pivot] + right
